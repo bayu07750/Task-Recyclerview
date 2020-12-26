883f2d22425339ce1b5dfb4a12e629c3858b1c30
bayu07750 <bayu07750@gmail.com>
2020-12-26 20:49:17 +0700

    create button and progressbar and when button clicked, progressbar is show

--- app/src/main/java/com/bayu07750/taskrecyclerview/ui/home/HomeActivity.kt
+++ app/src/main/java/com/bayu07750/taskrecyclerview/ui/home/HomeActivity.kt
@@ -1,51 +1,21 @@
 package com.bayu07750.taskrecyclerview.ui.home
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bayu07750.taskrecyclerview.R
 import com.bayu07750.taskrecyclerview.databinding.ActivityHomeBinding
-import com.bayu07750.taskrecyclerview.model.User
-import com.bayu07750.taskrecyclerview.ui.detail.DetailActivity
-import com.bayu07750.taskrecyclerview.utils.DataDummy
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var adapter: HomeAdapter
-    private lateinit var dataUser: List<User>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.title = getString(R.string.home)
-
-        dataUser = DataDummy.getPhoto
-        adapter = HomeAdapter(dataUser) { user ->
-            Intent(this, DetailActivity::class.java).apply {
-                putExtra(DetailActivity.KEY_EXTRA_USER, user)
-            }.also { startActivity(it) }
-        }
-
-        setupRecyclerview()
-    }
-
-    private fun setupRecyclerview() {
-        with(binding.recyclerview) {
-            setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(this@HomeActivity)
-            adapter = this@HomeActivity.adapter
-            addItemDecoration(
-                DividerItemDecoration(
-                    this@HomeActivity,
-                    RecyclerView.VERTICAL
-                )
-            )
+        binding.buttonShowProgressbar.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
         }
     }
 }
