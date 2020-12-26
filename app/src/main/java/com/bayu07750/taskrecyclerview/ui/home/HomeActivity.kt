c6619832beb5a1033388677ff38d02117be0281d
bayu07750 <bayu07750@gmail.com>
2020-12-26 20:58:22 +0700

    implementasi AlertDialog

--- app/src/main/java/com/bayu07750/taskrecyclerview/ui/home/HomeActivity.kt
+++ app/src/main/java/com/bayu07750/taskrecyclerview/ui/home/HomeActivity.kt
@@ -1,9 +1,10 @@
 package com.bayu07750.taskrecyclerview.ui.home
 
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bayu07750.taskrecyclerview.databinding.ActivityHomeBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class HomeActivity : AppCompatActivity() {
 
@@ -15,7 +16,21 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonShowProgressbar.setOnClickListener {
-            binding.progressBar.visibility = View.VISIBLE
+            createDialog()
         }
+    }
+
+    private fun createDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Konfirmasi")
+            .setMessage("Apakah anda ingin login?")
+            .setNegativeButton("Tidak") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton("Ya") { dialog, _ ->
+                Toast.makeText(this, "Silahkan tunggu Email dari kami", Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+            }
+            .show()
     }
 }
